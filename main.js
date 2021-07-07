ebe8d0e6c9be5ed0d3864bd113e06f3d79bba3c7
Supercoder5157 <76742451+Supercoder5157@users.noreply.github.com>
2021-07-07 11:20:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ function load(){
 recognition.onresult = function(event){
     console.log(event);
     var Content = event.results[0][0].transcript;
-    console.log(Content);
     document.getElementById("text_box").innerHTML = Content;
+    console.log(Content);
+    speak1();
 }
+
+
+function speak1(){
+    var synth = window.speechSynthesis;
+    speakdata = document.getElementById("text_box").value;
+    var utterThis = new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+} 
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width : 360,
+    height : 260,
+    image_format : "jpeg",
+    jpeg_quality : 90
+});
